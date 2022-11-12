835c3d3e24a63266716023af3bddf9e0fc25ed2d
Nutchapon <nutdeknon@gmail.com>
2022-11-12 14:08:58 +0700

    Initial Commit

---
+++ MyMapViewApp/MyMapViewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapViewAppApp.swift
+//  MyMapViewApp
+//
+//  Created by Student on 12/11/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct MyMapViewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
